e4431a0e3f6d6e10facc6c1cdfac6fb94957066c
hy18284 <hy18284@gmail.com>
2023-05-20 23:46:28 +0000

    Modify # of classes to be 12 in TOP

--- text_classification/scripts/train_top.sh
+++ text_classification/scripts/train_top.sh
@@ -4,7 +4,7 @@ for test_seed in 0 1 2 3 4 5 6 7 8 9
 do
     for test_id_ratio in 0.25 0.5 0.75
     do
-        test_n_labels=$(printf "%.0f"  $(echo  "16 * $test_id_ratio" | bc))
+        test_n_labels=$(printf "%.0f"  $(echo  "12 * $test_id_ratio" | bc))
         echo "# of test labels: $test_n_labels"
 
         python -m text_classification.train_text_classifier \
