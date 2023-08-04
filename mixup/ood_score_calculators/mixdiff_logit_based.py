3ecd20e2eed3c02cc537caa30494981b73f62314
hy18284 <hy18284@gmail.com>
2023-08-04 05:40:21 +0000

    Add similarity based weighted oracle averaging

--- mixup/ood_score_calculators/mixdiff_logit_based.py
+++ mixup/ood_score_calculators/mixdiff_logit_based.py
@@ -3,6 +3,7 @@ from typing import (
 )
 
 import torch
+import torch.nn.functional as F
 from clip.simple_tokenizer import SimpleTokenizer as clip_tokenizer
 import clip
 from tqdm import tqdm
@@ -19,18 +20,26 @@ class MixDiffLogitBasedMixin:
         batch_size: int,
         utilize_mixup: bool = True,
         add_base_score: bool = True,
+        selection_mode: str = 'argmax',
         intermediate_state: str = 'logit',
+        oracle_sim_mode: str = 'uniform',
+        oracle_sim_temp: float = 1.0,
         log_interval: Optional[int] = None,
     ):
         self.batch_size = batch_size
         self.utilize_mixup = utilize_mixup
         self.add_base_score = add_base_score
+        self.selection_mode = selection_mode
         self.intermediate_state = intermediate_state
+        self.oracle_sim_temp = oracle_sim_temp
+        self.oracle_sim_mode = oracle_sim_mode
 
         self.log_interval = log_interval
         self.known_mixup_table = wandb.Table(['x', 'y', 'mixup', 'rate', 'split'])
 
         assert self.intermediate_state in ('logit', 'softmax')
+        assert self.selection_mode in ('argmax', 'dot', 'euclidean')
+        assert self.oracle_sim_mode in ('uniform', 'l2', 'dot', 'cosine_sim')
     
     def load_model(self, backbone_name, device):
         self.clip_model, _ = clip.load(
@@ -64,6 +73,11 @@ class MixDiffLogitBasedMixin:
         self.prompts_embeds = self.clip_model.encode_text(prompts_ids)
         self.prompts_embeds /= torch.norm(self.prompts_embeds, dim=-1, keepdim=True)
         self.seen_labels = seen_labels
+
+        if self.selection_mode in ('euclidean', 'dot') or self.oracle_sim_mode != 'uniform':
+            NC, M, C, H, W = given_images.size()
+            given_images_flat = given_images.view(-1, C, H, W)
+            self.id_logits = self._process_mixup_images(given_images_flat)
 
         self.oracle_logits = [] 
         if self.ref_mode == 'oracle' or self.ref_mode == 'rand_id':
@@ -102,12 +116,21 @@ class MixDiffLogitBasedMixin:
             # [NC, (M * M * R, NC)] -> (NC, M * M * R, NC)
             self.oracle_logits = torch.stack(self.oracle_logits, dim=0)
 
+        self.NC = given_images.size(0)
+        self.M = len(given_images[0])
+        if self.ref_mode == 'rand_id':
+            self.P = len(ref_images)
+        elif self.ref_mode == 'oracle':
+            self.P = self.M
+        self.R = len(rates)
+
     def on_eval_end(self, iter_idx: int):
         del self.prompts_embeds
         del self.seen_labels
 
         self.id_logits = None
         self.oracle_logits = None
+        self.sim_oracle = None
 
         wandb.log({
             'oracle_mixup': self.known_mixup_table,
@@ -138,10 +161,38 @@ class MixDiffLogitBasedMixin:
         logits,
         **kwargs,
     ):
-        # (N)
-        max_indices = torch.argmax(logits, dim=-1)
-        # (NC, M, C, H, W) -> (N, M, C, H, W)
-        chosen_images = given_images[max_indices, ...]
+        if self.selection_mode == 'argmax':
+            # (N)
+            max_indices = torch.argmax(logits, dim=-1)
+            # (NC, M, C, H, W) -> (N, M, C, H, W)
+            chosen_images = given_images[max_indices, ...]
+        # elif self.selection_mode == 'euclidean' or self.selection_mode == 'dot':
+        #     # (N, NC), (NC * M, NC) -> (N, NC * M)
+        #     if self.selection_mode == 'euclidean':
+        #         dists = torch.cdist(logits, self.id_logits, p=2)
+        #     if self.selection_mode == 'dot':
+        #         dists = -(logits @ self.id_logits.t())
+        #     # (N, NC * M) -> (N, M)
+        #     _, topk_indices = torch.topk(
+        #         dists, 
+        #         dim=1, 
+        #         k=len(given_images[0]),
+        #         largest=False,
+        #         sorted=True,
+        #     )
+        #     # (N, M), (NC, M) -> (N, M)
+
+        #     # (NC, M, C, H, W) -> (NC * M, C, H, W)
+        #     given_images = torch.flatten(given_images, 0, 1)
+        #     given_images = torch.gather(given_images, 1, topk_indices)
+        # else:
+        #     ValueError('Invalid selection option.')
+
+        if self.oracle_sim_mode != 'uniform':
+            # (NC, M, NC) -> (N, M, NC)
+            id_logits = self.id_logits.view(self.NC, self.M, self.NC)
+            self.sim_oracle = id_logits[max_indices, ...]
+
         return chosen_images
     
     @torch.no_grad()
@@ -168,6 +219,52 @@ class MixDiffLogitBasedMixin:
             mixed_logits = self._process_mixup_images(images)
         else:
             raise ValueError(f'Invalid ref_mode: {self.ref_mode}')
+
+        N, NC = logits.size()
+        if self.oracle_sim_mode == 'uniform':
+            return mixed_logits
+
+        if self.ref_mode not in ('oracle', 'rand_id'):
+            raise ValueError('Not yet implemented for other ref_modes')
+
+        if self.ref_mode == 'in_batch':
+            self.P = len(logits)
+        
+        if self.oracle_sim_mode == 'l2':
+            # (N, 1, NC), (N, M, NC) -> (N, 1, M) -> (N, M)
+            sim_scores = -torch.cdist(logits.unsqueeze(1), self.sim_oracle, p=2)
+            sim_scores = sim_scores.squeeze()
+        elif self.oracle_sim_mode == 'dot':
+            # (N, 1, NC), (N, M, NC) -> (N, M, NC) -> (N, M)
+            sim_scores = logits.unsqueeze(1) * self.sim_oracle
+            sim_scores = sim_scores.sum(dim=-1)
+        elif self.oracle_sim_mode == 'cosine_sim':
+            # (N, 1, NC), (N, M, NC) -> (N, M)
+            sim_scores = F.cosine_similarity(
+                logits.unsqueeze(1), 
+                self.sim_oracle, 
+                dim=-1,
+            )
+
+        # (N,  M,  P,  R, NC)
+        mixed_logits = mixed_logits.view(N, self.M, self.P, self.R, NC)
+
+        # (N, M, 1) -> (N, M, P)
+        sim_scores = sim_scores.unsqueeze(-1).repeat(1, 1, self.P)
+        if self.ref_mode == 'oracle':
+            diags = torch.diagonal(sim_scores, dim1=1, dim2=2)
+            diags.fill_(torch.tensor(float('-inf'), device=self.device))
+        sim_scores = torch.softmax(sim_scores / self.oracle_sim_temp, dim=1)
+
+        # (N, M, P, R, NC) * (N, M, P, 1, 1) -> (N, M, P, R, NC)
+        mixed_logits = mixed_logits * sim_scores[:, :, :, None, None]
+        
+        # Make sure that proper average can be taken.
+        if self.ref_mode == 'oracle':
+            mixed_logits = mixed_logits * (self.M - 1)
+        else:
+            mixed_logits = mixed_logits * self.M
+
         return mixed_logits
 
     @torch.no_grad()
