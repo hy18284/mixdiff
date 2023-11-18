1c1cc15c1601846209c750536cda9abd1439f6d8
hy18284 <hy18284@gmail.com>
2023-11-18 10:42:55 +0000

    Add MixDiff adv clean scripts

--- mixup/scripts/adversarial/entropy_orc_mixdiff_clean.sh
+++ mixup/scripts/adversarial/entropy_orc_mixdiff_clean.sh
@@ -2,11 +2,10 @@
 
 for intermediate_state in softmax
 do
-    for ref_mode in in_batch
+    for ref_mode in oracle
     do
         for method in \
-            MixDiffEntropy \
-            MixDiffMaxSofmaxProb 
+            MixDiffEntropy 
         do 
             for dataset in \
                 CIFAR100OODDataset \
@@ -14,20 +13,20 @@ do
             do
                 for eps in 1
                 do
-                for attack_nb_iter in 10 1 5 20 50 100
+                for attack_nb_iter in 10
                 do
-                    for attack in id2ood both ood2id
+                for attack in none
+                do
+                    for gamma in 2.0
                     do
-                        for gamma in 1.0
+                        for n in 100
                         do
-                            for n in 15
+                            for p in 3
                             do
-                                for p in 3
+                                for m in 15
                                 do
-                                    for m in 15
+                                    for r in 7
                                     do
-                                        for r in 3
-                                        do
                                             if [ "$method" = "MixDiffEnergy" ] || [ "$method" = "MixDiffMaxLogitScore" ] && [ "$intermediate_state" = "softmax" ]; then
                                                 echo "Skipping ${method} ${intermediate_state}"
                                                 continue
@@ -40,7 +39,7 @@ do
                                                 --gamma $gamma \
                                                 --r_ref 0 \
                                                 --seed 0 \
-                                                --wandb_name "adv_$attack" \
+                                                --wandb_name adv_$attack \
                                                 --wandb_project ZOC \
                                                 --wandb_tags adv \
                                                 --device 0 \
@@ -58,14 +57,15 @@ do
                                                 --score_calculator.init_args.intermediate_state $intermediate_state \
                                                 --score_calculator.init_args.oracle_sim_mode uniform \
                                                 --score_calculator.init_args.oracle_sim_temp 1.0 \
-                                                --score_calculator.init_args.utilize_mixup false \
+                                                --score_calculator.init_args.utilize_mixup true \
                                                 --score_calculator.init_args.selection_mode argmax \
-                                                --score_calculator.init_args.add_base_score true \
+                                                --score_calculator.init_args.add_base_score false \
                                                 --fnr_at 0.95 \
                                                 --fpr_at 0.95 \
                                                 --log_interval null \
                                                 --datamodule.class_path mixup.ood_datamodules.$dataset \
                                                 --mixup_operator.class_path mixup.mixup_operators.InterpolationMixup
+                                            done
                                         done
                                     done
                                 done
@@ -77,4 +77,10 @@ do
         done
     done
 done
-done
+                # Caltech101OODDataset
+                                                # --datamodule.init_args.val_ratio 0.66 \
+                                        # for attack in id2ood ood2id both none
+                # CIFAR10OODDataset
+                # CIFARPlus50OODDataset \
+                # TinyImageNetOODDataset
+                # for attack in none id2ood ood2id both
