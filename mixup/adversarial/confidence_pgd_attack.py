d70ea3e2d4bb8a0de78f112893a6f7ce977cceb4
hy18284 <hy18284@gmail.com>
2023-11-20 09:24:48 +0000

    Modify ID2OOD attack from mean to sum

--- mixup/adversarial/confidence_pgd_attack.py
+++ mixup/adversarial/confidence_pgd_attack.py
@@ -29,7 +29,7 @@ class OELoss(nn.Module):
         super(OELoss, self).__init__()
 
     def forward(self, x):
-        return -(x.mean(1) - torch.logsumexp(x, dim=1)).mean()
+        return -(x.mean(1) - torch.logsumexp(x, dim=1)).sum()
 
 class ConfidenceLinfPGDAttack:
     """
