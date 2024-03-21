8484467aa1dc1456e157130ccbac3d128adf5e7c
hy18284 <hy18284@gmail.com>
2024-03-21 17:48:49 +0000

    Add gaussian adv script

---
+++ mixup/scripts/adversarial/entropy_gaussian.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+for intermediate_state in softmax
+do
+    for ref_mode in rand_id
+    do
+        for method in \
+            MixDiffEntropy
+        do 
+            for dataset in \
+                CIFAR100OODDataset
+            do
+                for eps in 1
+                do
+                    for attack_nb_iter in 10 
+                    do
+                        for attack in id2ood ood2id both
+                        do
+                            for gamma in 2.0
+                            do
+                                for m in 15
+                                do
+                                    for p in 15
+                                    do
+                                        for vars in "0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001"
+                                        do
+                                            if [ "$method" = "MixDiffEnergy" ] || [ "$method" = "MixDiffMaxLogitScore" ] && [ "$intermediate_state" = "softmax" ]; then
+                                                echo "Skipping ${method} ${intermediate_state}"
+                                                continue
+                                            fi
+                                            python -m mixup.mixup_eval_text \
+                                                --n 100 \
+                                                --m $m \
+                                                --r 7 \
+                                                --p 9 \
+                                                --gamma $gamma \
+                                                --r_ref 0 \
+                                                --seed 0 \
+                                                --wandb_name $attack \
+                                                --wandb_project ZOC_debug \
+                                                --wandb_tags adv \
+                                                --device 0 \
+                                                --ref_mode $ref_mode \
+                                                --model_path 'ViT-B/32' \
+                                                --max_samples null \
+                                                --attack $attack \
+                                                --attack_eps $eps \
+                                                --attack_nb_iter $attack_nb_iter \
+                                                --trans_before_mixup false \
+                                                --score_calculator.class_path mixup.ood_score_calculators.$method \
+                                                --score_calculator.init_args.backbone.class_path mixup.ood_score_calculators.backbones.ClipBackbone \
+                                                --score_calculator.init_args.batch_size 5000 \
+                                                --score_calculator.init_args.log_interval null \
+                                                --score_calculator.init_args.intermediate_state $intermediate_state \
+                                                --score_calculator.init_args.oracle_sim_mode uniform \
+                                                --score_calculator.init_args.oracle_sim_temp 1.0 \
+                                                --score_calculator.init_args.utilize_mixup true \
+                                                --score_calculator.init_args.selection_mode argmax \
+                                                --score_calculator.init_args.add_base_score true \
+                                                --fnr_at 0.95 \
+                                                --fpr_at 0.95 \
+                                                --log_interval null \
+                                                --datamodule.class_path mixup.ood_datamodules.$dataset \
+                                                --mixup_operator.class_path mixup.mixup_operators.GaussianNoise \
+                                                --mixup_operator.init_args.stds "[$vars]"
+                                            done
+                                        done
+                                    done
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
