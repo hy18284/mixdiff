b26905159d8f5ea7aefa9309e597905c6a2eef66
hy18284 <hy18284@gmail.com>
2024-06-19 00:29:00 +0900

    Add diff model scripts

--- mixup/scripts/diff_model/oracle_caltech.sh
+++ mixup/scripts/diff_model/oracle_caltech.sh
@@ -2,16 +2,20 @@
 for ref_mode in oracle
 do
     for method in \
+        MixDiffMaxSofmaxProb \
         MixDiffEntropy
     do 
         for dataset in \
-            CIFAR10OODDataset
+            Caltech101OODDataset
         do
-            for gamma in 2.0 1.0 0.5
+            for backbone in \
+                RN50 \
+                ViT-L/14 \
+                RN50x64
             do
-                for m in 15 10
+                for gamma in 2.0 1.0 0.5
                 do
-                    for p in 14 9
+                    for m in 15 10
                     do
                         for r in 7 5
                         do
@@ -23,28 +27,29 @@ do
                                 --n 10 \
                                 --m $m \
                                 --r $r \
-                                --p $p \
+                                --p $m \
                                 --gamma $gamma \
                                 --r_ref 0 \
                                 --seed 0 \
-                                --wandb_name 'miscls' \
-                                --wandb_project ZOC_dev \
-                                --wandb_tags miscls_v1 \
+                                --wandb_name $backbone \
+                                --wandb_project ZOC \
+                                --wandb_tags diff_model_v1 \
                                 --device 0 \
-                                --model_path 'ViT-B/32' \
+                                --model_path $backbone \
                                 --max_samples null \
                                 --trans_before_mixup true \
                                 --ref_mode $ref_mode \
                                 --score_calculator.class_path mixup.ood_score_calculators.$method \
                                 --score_calculator.init_args.backbone.class_path mixup.ood_score_calculators.backbones.ClipBackbone \
-                                --score_calculator.init_args.batch_size 100 \
+                                --score_calculator.init_args.batch_size 500 \
                                 --score_calculator.init_args.log_interval null \
                                 --score_calculator.init_args.intermediate_state softmax \
                                 --fnr_at 0.95 \
                                 --fpr_at 0.95 \
                                 --log_interval null \
-                                --max_samples 100 \
                                 --datamodule.class_path mixup.ood_datamodules.$dataset \
+                                --datamodule.init_args.val_ratio 0.66 \
+                                --datamodule.init_args.with_replacement true  \
                                 --mixup_operator.class_path mixup.mixup_operators.InterpolationMixup
                         done
                     done
@@ -53,4 +58,3 @@ do
         done
     done
 done
-        # MixDiffMaxSofmaxProb \
